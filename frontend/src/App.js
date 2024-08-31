f278c57fc95f16d332b253cd1716fb2bac92a467
CornedeLange <161510876+CornedeLange@users.noreply.github.com>
2024-08-31 12:38:46 +0200

    Trying to figure out Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Splash from "./pages/Splash";
+import Playlist from "./pages/Playlist";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/profile",
+        element: <Profile/>
+    },
+    {
+        path: "/splash",
+        element: <Splash/>
+
+    },
+    {
+        path: "/playlist",
+        element: <Playlist/>
+    }
+])
 
 class App extends React.Component{
     render(){
         return (
-            <div>
+            <RouterProvider router={router}>
                 <Navigation />
-            </div>
+            </RouterProvider>
         );
     }
 }
+
+export default App;
